18c5fa0dc4f9787e5038f20f20ee5430e41a1452
Andreas-debug <53007751+Andreas-debug@users.noreply.github.com>
2019-08-14 09:02:38 +0200

    Add files via upload

--- mk1.js
+++ mk1.js
@@ -1,5 +1,6 @@
 function imageData(image) 
 {
+    //document.write(image.src);
     var canvas = document.createElement('canvas');
     canvas.width = image.width;
     canvas.height = image.height;
@@ -9,15 +10,15 @@ function imageData(image)
 
 }
 
-function getPixel( context, x, y ) {
+function getPixel(context, x, y){
     const ata = context.getImageData(x, y, 1, 1).data;
     document.getElementById("asciiArt").innerHTML+=ata;
     //document.write(ata + 'RGB' + '\n\n\n');
-    console.print
     return ata; 
 
 }
-function readURL(input) {
+function readURL(input){
+    var inp = new Image(input.files[0].width, input.files[0].height);
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         
@@ -26,21 +27,18 @@ function readURL(input) {
                 .attr('src', e.target.result)
                 .width(input.files[0].width)
                 .height(input.files[0].height);
+            inp.src = e.target.result;
         };
         reader.readAsDataURL(input.files[0]);
     }
-
-    var inp = new Image(document.getElementById("blah").width, document.getElementById("blah").height);
-    inp.src = document.getElementById("blah").src;
-
     var imagedata = imageData(inp);
     for(var y = 0; y < inp.height; y++)
     {
         for(var x = 0; x < inp.width; x++)
         {
-            var color = getPixel( imagedata, x, y );
+            var color = getPixel(imagedata, x, y);
             var brightness = ((color[0] + color[1] + color[2])/3)*(color[3]/255);
-            //document.write(color);
+            document.write(color);
         }
     }
 }
